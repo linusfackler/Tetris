86005e57b7328e5cea7d09befcfd0611041f0f87
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-06-05 12:33:14 +0200

    update

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,11 +4,14 @@ using UnityEngine.Tilemaps;
 public class Board : MonoBehaviour
 {
     public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominoes;
+    public Vector3Int spawnPosition;
 
     private void Awake()
     {
         this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
 
         for (int i = 0; i < this.tetrominoes.Length; i++)
             this.tetrominoes[i].Initialize();
@@ -23,10 +26,17 @@ public class Board : MonoBehaviour
     {
         int random = Random.Range(0, this.tetrominoes.Length);
         TetrominoData data = this.tetrominoes[random];
+
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(this.activePiece);
     }
 
-    public void Set()
+    public void Set(Piece piece)
     {
-        
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
     }
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Board board         { get; private set; }
+    public TetrominoData data  { get; private set; }
+    public Vector3Int position { get; private set; }
+    public Vector3Int[] cells  { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
-        
+        this.board = board;
+        this.position = position;
+        this.data = data;
+
+        if (this.cells == null)
+            this.cells = new Vector3Int[data.cells.Length];
+        for (int i = 0; i < data.cells.Length; i++)
+            this.cells[i] = (Vector3Int)data.cells[i];
     }
 }
