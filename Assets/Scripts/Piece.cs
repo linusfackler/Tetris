f705e959ca085aaf6c41eb974b87f200d64f3f0d
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-06-20 23:15:34 +0200

    update

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -78,7 +78,6 @@ public class Piece : MonoBehaviour
     public void RotateLeft(InputAction.CallbackContext context)
     {
         Rotate(-1);
-        print("Rotated left");
     }
 
     public void RotateRight(InputAction.CallbackContext context)
